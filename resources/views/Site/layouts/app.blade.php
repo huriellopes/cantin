86ef9dcda9cac5407bb67a51fa2159e35c2bad22
Huriel Lopes <huriellopes1996@gmail.com>
2025-01-16 21:19:17 -0300

    Adjustment in layout

--- resources/views/Site/layouts/app.blade.php
+++ resources/views/Site/layouts/app.blade.php
@@ -7,7 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="shortcut icon" href="{{ asset('/assets/images/cantin.ico') }}" type="image/x-icon" />
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>CaNTIn - Cadastro Nacional de Terreiros Trans-Inclusivos</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simple-notify@0.5.5/dist/simple-notify.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
