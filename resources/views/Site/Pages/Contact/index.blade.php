315e1206df34b5e3ed992178ba5f321502b8a83e
Huriel Lopes <huriellopes1996@gmail.com>
2025-01-24 11:40:21 -0300

    Adjustment in layout page contact!

--- resources/views/Site/Pages/Contact/index.blade.php
+++ resources/views/Site/Pages/Contact/index.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div class="row mt-5">
-        <div class="col mt-2">
+        <div class="col mt-3">
             <h2 class="text-center">Contato</h2>
         </div>
     </div>
 
     <div class="row mt-2">
-        <div class="col">
+        <div class="col-12 col-md-6">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245795.98240152583!2d-48.10251678450125!3d-15.721369541721826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3d18df9ae275%3A0x738470e469754a24!2zQnJhc8OtbGlhLCBERg!5e0!3m2!1spt-BR!2sbr!4v1700220418984!5m2!1spt-BR!2sbr"
-                width="600"
+                width="100%"
                 height="450"
                 style="border: solid 1px #000000;"
                 allowfullscreen=""
@@ -19,7 +19,7 @@
                 referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
         </div>
-        <div class="col">
+        <div class="col-12 col-md-6">
             <p>
                 <strong>Babalorixá Alan T'Ogun</strong>
             </p>
